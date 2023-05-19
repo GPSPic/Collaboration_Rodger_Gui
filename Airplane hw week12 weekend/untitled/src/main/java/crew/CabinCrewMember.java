7b5b262acc1bac71af723147b3eabad49e2aab09
GPSPic <picard.guillaume47@gmail.com>
2023-05-19 11:44:13 +0100

    hw class creation

---
+++ Airplane hw week12 weekend/untitled/src/main/java/crew/CabinCrewMember.java
@@ -0,0 +1,12 @@
+package crew;
+
+public class CabinCrewMember {
+
+    private String name;
+    private Rank rank;
+
+    public CabinCrewMember(String name, Rank rank) {
+        this.name = name;
+        this.rank = rank;
+    }
+}
