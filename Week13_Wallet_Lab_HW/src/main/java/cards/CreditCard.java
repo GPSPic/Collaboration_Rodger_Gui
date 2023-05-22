093362be433558774ac6d21bf0d235ddd5704e6c
GPSPic <picard.guillaume47@gmail.com>
2023-05-22 15:27:00 +0100

    CreditCard test complete

--- Week13_Wallet_Lab_HW/src/main/java/cards/CreditCard.java
+++ Week13_Wallet_Lab_HW/src/main/java/cards/CreditCard.java
@@ -2,13 +2,17 @@ package cards;
 
 import payment.Chargeable;
 
+import java.util.ArrayList;
+
 public class CreditCard extends PaymentCard implements Chargeable {
 
     private double availableCredit;
+//    private ArrayList<String> transactions;
 
     public CreditCard(int cardNumber, String expiryDate, int securityNo, double availableCredit){
         super(cardNumber, expiryDate, securityNo);
         this.availableCredit = availableCredit;
+//        this.transactions = new ArrayList<>();
     }
 
     public double getAvailableCredit() {
@@ -19,9 +23,21 @@ public class CreditCard extends PaymentCard implements Chargeable {
         this.availableCredit = availableCredit;
     }
 
-    public double
+    public double addFee(double transaction) {
+        return transaction += transaction * 0.05;
+    }
+
+    public void reduceCredit(double totalPrice) {
+        if (this.availableCredit >= totalPrice) {
+            this.availableCredit -= totalPrice;
+        } else {
+            System.out.println("You pauper!");
+        }
+    }
     
     public void charge(double transactionAmount) {
-
+        double totalPrice = this.addFee(transactionAmount);
+        this.reduceCredit(totalPrice);
+        this.logTransaction(String.format("You paid %s for some junk", totalPrice));
     }
 }
