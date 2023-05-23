ccca914767afe0875d7c3a96e167367c06a34b3d
GPSPic <picard.guillaume47@gmail.com>
2023-05-23 15:27:18 +0100

    create new engine each time

--- week13_Car_Dealership_Lab/CarDealership_Lab/src/main/java/cars/ElectricCar.java
+++ week13_Car_Dealership_Lab/CarDealership_Lab/src/main/java/cars/ElectricCar.java
@@ -4,10 +4,11 @@ import engines.ElectricEngine;
 
 public class ElectricCar extends Car {
 
-    private ElectricEngine electricEngine = new ElectricEngine();
+//    private ElectricEngine electricEngine = new ElectricEngine();
 
     public ElectricCar(String name, double price){
         super(name, price);
+        ElectricEngine electricEngine = new ElectricEngine();
         this.addEngine(electricEngine);
     }
 
--- week13_Car_Dealership_Lab/CarDealership_Lab/src/main/java/cars/PetrolCar.java
+++ week13_Car_Dealership_Lab/CarDealership_Lab/src/main/java/cars/PetrolCar.java
@@ -7,10 +7,11 @@ import engines.PetrolEngine;
 public class PetrolCar extends Car{
 
 //    private ArrayList<Engine> engines;
-    private PetrolEngine petrolEngine = new PetrolEngine();
+//    private PetrolEngine petrolEngine = new PetrolEngine();
 
     public PetrolCar(String make, double price){
         super(make, price);
+        PetrolEngine petrolEngine = new PetrolEngine();
         this.addEngine(petrolEngine);
     }
 
