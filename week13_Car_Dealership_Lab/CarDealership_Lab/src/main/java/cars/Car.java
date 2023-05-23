421881e55c4926aa9fbb430648866d5a143863de
GPSPic <picard.guillaume47@gmail.com>
2023-05-23 13:01:49 +0100

    create car class and methods

--- week13_Car_Dealership_Lab/CarDealership_Lab/src/main/java/cars/Car.java
+++ week13_Car_Dealership_Lab/CarDealership_Lab/src/main/java/cars/Car.java
@@ -1,9 +1,48 @@
 package cars;
+import engines.Engine;
 
 import java.util.ArrayList;
 
 public abstract class Car {
 
-//    private ArrayList
+    private String make;
+    private ArrayList<Engine> engines;
+    private double price;
+    private double damageValue;
 
+    public Car(String make, ArrayList<Engine> engines, double price){
+        this.make = make;
+        this.price = price;
+        this.damageValue = 0;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public ArrayList<Engine> getEngines() {
+        return engines;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getDamageValue() {
+        return damageValue;
+    }
+
+    public void setDamageValue(double damageValue) {
+        this.damageValue += damageValue;
+    }
+
+    public void sufferDamage(int damageValue) {
+        this.setDamageValue(damageValue);
+        this.price -= this.damageValue;
+    }
+
+    public void repairCar() {
+        this.price += this.damageValue;
+        this.setDamageValue(0);
+    }
 }
---
+++ week13_Car_Dealership_Lab/CarDealership_Lab/src/main/java/engines/ElectricEngine.java
@@ -0,0 +1,8 @@
+package engines;
+
+public class ElectricEngine extends Engine {
+
+    public ElectricEngine(){
+        super(FuelType.ELECTRICITY);
+    }
+}
