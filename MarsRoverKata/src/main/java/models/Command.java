f0f920acc9782fbe86c88d51e2d4e19d3a60fd79
GPSPic <picard.guillaume47@gmail.com>
2023-07-26 14:28:29 +0100

    attempt at sorting command

--- MarsRoverKata/src/main/java/models/Command.java
+++ MarsRoverKata/src/main/java/models/Command.java
@@ -8,10 +8,14 @@ public class Command {
 
     private String commandLine;
     private List<String> sortedCommands;
+    private List<String> plateauCommand;
+    private List<List<String>> roverCommands;
 
     public Command(String commandLine) {
         this.commandLine = commandLine;
         this.sortedCommands = new ArrayList<>();
+        this.plateauCommand = new ArrayList<>();
+        this.roverCommands = new ArrayList<>();
     }
 
     public String getCommandLine() {
@@ -30,18 +34,77 @@ public class Command {
         this.sortedCommands = sortedCommands;
     }
 
+    public List<String> getPlateauCommand() {
+        return plateauCommand;
+    }
+
+    public void setPlateauCommand(List<String> plateauCommand) {
+        this.plateauCommand = plateauCommand;
+    }
+
+    public List<List<String>> getRoverCommands() {
+        return roverCommands;
+    }
+
+    public void setRoverCommands(List<List<String>> roverCommands) {
+        this.roverCommands = roverCommands;
+    }
+
     public void addSortedCommand(String sortedCommand) {
         this.sortedCommands.add(sortedCommand);
     }
 
-    public void sortCommand() {
-        String noSpaceCommand = this.commandLine.replaceAll("[ ]+", "");
-        String[] commandsAsArrays = noSpaceCommand.split("\\n+");
-        List<String> commandsAsList = Arrays.asList(commandsAsArrays);
-        this.setSortedCommands(commandsAsList);
+//    public void sortCommand() {
+//        String noSpaceCommand = this.commandLine.replaceAll("[ ]+", "");
+//        String[] commandsAsArrays = noSpaceCommand.split("\\n+");
+//        List<String> commandsAsList = Arrays.asList(commandsAsArrays);
+//        this.setSortedCommands(commandsAsList);
 //        int listSize = commandsAsList.size();
 //        for (int i = 0; i < listSize; i++) {
 //            this.sortedCommands.add(commandsAsList.get(i));
 //        }
+//    }
+
+    public boolean directionFinder(String str) {
+        if (
+            str.matches("N|E|S|W")
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public void sortCommand() {
+//        String noSpaceCommand = this.commandLine.replaceAll("[ ]+", "");
+        String noSpaceCommand = this.commandLine.replaceAll("\\s+", "");
+        String[] commandsAsArrays = noSpaceCommand.split("");
+        List<String> commandsAsList = Arrays.asList(commandsAsArrays);
+
+        List<String> parsedPlateauCommand = commandsAsList.subList(0, 2);
+        this.setPlateauCommand(parsedPlateauCommand);
+
+        List<List<String>> roversCommand = new ArrayList<>();
+        List<String> spreadRoversCommand = commandsAsList.subList(2, commandsAsList.size());
+
+        for (int i = 2; i < spreadRoversCommand.size(); i++) {
+            if (this.directionFinder(spreadRoversCommand.get(i))) {
+                int commandStart = i - 2;
+                for (int j = i + 1; j < spreadRoversCommand.size(); j++) {
+                    try {
+                        Integer.parseInt(spreadRoversCommand.get(j));
+                        int commandEnd = j;
+                        List<String> individualRoverCommand = spreadRoversCommand.subList(commandStart, commandEnd);
+                        roversCommand.add(individualRoverCommand);
+                    }
+                    catch (NumberFormatException nfe) {
+                        System.out.println("Attempt number: " + Integer.toString(j));
+                    }
+                }
+            }
+
+        }
+        this.setRoverCommands(roversCommand);
     }
 }
--- MarsRoverKata/src/test/java/CommandTest.java
+++ MarsRoverKata/src/test/java/CommandTest.java
@@ -2,12 +2,13 @@ import models.Command;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 
 public class CommandTest {
 
     private Command command1;
+    private Command commandGoal;
 
     @Before
     public void before(){
@@ -16,17 +17,36 @@ public class CommandTest {
                 "1 2 N\n" +
                 "\n" +
                 "RM");
+        commandGoal = new Command(
+                "5 5\n" +
+                        "\n" +
+                        "1 2 N\n" +
+                        "\n" +
+                        "LMLMLMLMM\n" +
+                        "\n" +
+                        "3 3 E\n" +
+                        "\n" +
+                        "MMRMMRMRRM\n");
+
     }
 
     @Test
     public void canSortCommand() {
-        command1.sortCommand();
-        assertEquals(3, command1.getSortedCommands().size());
+        commandGoal.sortCommand();
+//        assertEquals(3, command1.getSortedCommands().size());
+        System.out.println("plateauCommand: " + commandGoal.getPlateauCommand());
+        System.out.println("current rovers command: " + commandGoal.getRoverCommands());
     }
 
-//    @Test
-//    public void canProcessCommand(){
-//
-//    }
+    @Test
+    public void canFindDirection(){
+        assertTrue(command1.directionFinder("N"));
+        assertTrue(command1.directionFinder("E"));
+        assertTrue(command1.directionFinder("S"));
+        assertTrue(command1.directionFinder("W"));
+        assertFalse(command1.directionFinder("n"));
+        assertFalse(command1.directionFinder("F"));
+        assertFalse(command1.directionFinder("NE"));
+    }
 
 }
